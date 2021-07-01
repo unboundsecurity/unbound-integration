611f2f04f4dc97c59a0c6c933fb3473eb9a7be7b
mdvir <michaeld1427@gmail.com>
2021-07-01 15:42:07 +0300

    container work with ukc and c# program

--- microsoft-365-dke/.devcontainer/data/start.sh
+++ microsoft-365-dke/.devcontainer/data/start.sh
@@ -2,10 +2,17 @@
 # This script is used in the postCreateCommand of the Visual Studio Code Dev Container
 set -e
 
+# configure env params
+export EP_HOST_NAME="ep1:8443"
+export KC_CRYPTO_USER="encrypter"
+export UKC_CRYPTO_USER_PASSWORD="Password1!"
+export UKC_PARTITION="test"
+export UKC_SO_PASSWORD="Unbound1!"
+export UKC_PASSWORD="Unbound1!"
+
 echo "servers=$EP_HOST_NAME">/etc/ekm/client.conf
 
-echo "54.158.110.92 ep1" >> /etc/hosts
-
+echo "107.22.151.1 ep1" >> /etc/hosts
 
 # Wait until UKC is ready
 sh /root/data/wait_for_ukc_cluster_to_start.sh
@@ -13,9 +20,11 @@ sh /root/data/wait_for_ukc_cluster_to_start.sh
 sh /root/data/create_partition.sh
 # Register UKC client - establish secure connection with PKCS11 
 #sh /root/data/register_new_client_ephemeral.sh
- sh /root/data/register_new_client.sh
+sh /root/data/register_new_client.sh
+cd /root/data/published
+dotnet customerkeystore.dll
 
 #
 #sh /root/data/run_encrypt_demo.sh
 
-# tail -f /dev/null #keep container running
+tail -f /dev/null #keep container running
--- microsoft-365-dke/.devcontainer/run.sh
+++ microsoft-365-dke/.devcontainer/run.sh
@@ -9,3 +9,6 @@ docker run --name ukc-client \
         --network=vhsm_unbound \
         --env-file=env \
         -t -i unboundukc/ukc-client:demo-java-encrypt
+
+ #command to run docker image
+#docker run -dit --env EP_HOST_NAME=ep1:8443 --env UKC_PARTITION:test --env UKC_SO_PASSWORD:Unbound1! --env UKC_PASSWORD:Unbound1! vsc-microsoft-365-dke-e97c3a9132f476c39eebfcec6a51c345 /root/data/start.sh
