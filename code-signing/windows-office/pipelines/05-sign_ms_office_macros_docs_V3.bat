f4969e5b38e6fad619d7afaa829df6b6b9af69e0
Sasham111 <53482897+Sasham111@users.noreply.github.com>
2021-08-29 22:49:46 +0300

    Add files via upload

--- code-signing/windows-office/pipelines/05-sign_ms_office_macros_docs_V3.bat
+++ code-signing/windows-office/pipelines/05-sign_ms_office_macros_docs_V3.bat
@@ -7,17 +7,15 @@ if not exist "..\signed_files" mkdir "..\signed_files"
 rem Sign macros of Excel document
 copy ..\files_to_sign\book_with_macro.xlsm ..\signed_files\book_with_macro_signed.xlsm
 echo Signing macros of Excel document in ..\signed_files\book_with_macro_signed.xlsm
-call offsign_ub_ksp_runner.bat -i ..\signed_files\book_with_macro_signed.xlsm -c %cert% -p test -u sasha -w Password1!
-rem %my_signtool% sign /v /t http://timestamp.digicert.com /fd sha256 /n %cert% "..\signed_files\book_with_macro_signed.xlsm"
+call offsign_ub_ksp_runner.bat -i ..\signed_files\book_with_macro_signed.xlsm -c %cert% -p test -u sasha -w Password1! -t http://timestamp.digicert.com
 
 rem Sign macros of Word document
 echo Signing macros of Word document in ..\signed_files\word_doc_with_macros_signed.docm
 copy ..\files_to_sign\word_doc_with_macros.docm ..\signed_files\word_doc_with_macros_signed.docm
-call offsign_ub_ksp_runner.bat -i ..\signed_files\word_doc_with_macros_signed.docm -c %cert% -p test -u sasha -w Password1!
-rem %my_signtool% sign /v /t http://timestamp.digicert.com /fd sha256 /n %cert% "..\signed_files\word_doc_with_macros_signed.docm"
+call offsign_ub_ksp_runner.bat -i ..\signed_files\word_doc_with_macros_signed.docm -c %cert% -p test -u sasha -w Password1! -t http://timestamp.digicert.com
 
 rem Verify signed documents
-echo Verifying signature of the signed macros 
-set my_signtool="C:\Program Files (x86)\Windows Kits\10\bin\10.0.20348.0\x86\signtool.exe"
-%my_signtool% verify /pa "..\signed_files\book_with_macro_signed.xlsm"
-%my_signtool% verify /pa "..\signed_files\word_doc_with_macros_signed.docm"
+rem echo Verifying signature of the signed macros 
+rem set my_signtool="C:\Program Files (x86)\Windows Kits\10\bin\10.0.20348.0\x86\signtool.exe"
+rem %my_signtool% verify /pa "..\signed_files\book_with_macro_signed.xlsm"
+rem %my_signtool% verify /pa "..\signed_files\word_doc_with_macros_signed.docm"
