8fca31eaef10f0e3ffa10b0280d781fb65f58e11
Yuli <stremovsky@gmail.com>
2021-10-05 11:18:21 +0300

    add example

--- linux-volume-encryption/encfs-aws-terraform/variables.tf
+++ linux-volume-encryption/encfs-aws-terraform/variables.tf
@@ -32,7 +32,13 @@ variable "rsa_key_name" { default = "%KEYNAME%" }
 # wget ekm-client command
 variable "wget_ekm_client" { default = "wget --no-verbose --no-check-certificate https://unbound-ekm-client.s3.us-west-1.amazonaws.com/ekm-client-2.0.2010.38364.el8.x86_64.rpm -O /tmp/ekm-client.rpm" }
 
-# Generate enceypted keyphase used by encfs
+# Generate encrypted keyphase used by encfs
+# METHOD 1
+# head -c 128 /dev/random > /tmp/rnd.txt
+# ucl encrypt -i /tmp/rnd.txt -o /tmp/enc.txt -p code-sign -n rsakey1
+# rm -rf /tmp/rnd.txt
+# base64 /tmp/enc.txt | tr -d "\n"
+# METHOD 2
 # 1. Extract public key from Unbound UKC
 # ucl export -n aaa --output cert.pub
 # 2. Encrypted keyphase using the public key
